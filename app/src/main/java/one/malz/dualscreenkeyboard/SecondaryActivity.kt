67fab34d56cb1e2619e95a23b41438d78427fa82
Markus Malz <markus.malz@outlook.com>
2021-03-05 16:34:44 +0100

    whoooo

--- app/src/main/java/one/malz/dualscreenkeyboard/SecondaryActivity.kt
+++ app/src/main/java/one/malz/dualscreenkeyboard/SecondaryActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.Display
+import android.view.View
 import androidx.annotation.RequiresApi
 import one.malz.dualscreenkeyboard.databinding.ActivitySecondaryBinding
 import java.lang.Exception
@@ -25,6 +26,23 @@ class SecondaryActivity(outerContext: Context?, display: Display?, theme: Int) :
         setTitle("Dual Screen Keyboard Editor")
         binding.editTextTextMultiLine.setBackgroundColor(Color.BLACK)
         binding.editTextTextMultiLine.setTextColor(Color.WHITE);
+
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window!!.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 
     fun setText(s: String) {
